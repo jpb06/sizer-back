563668bf9c2e436ab8501bb1b5fd15cdadd47a76
jpb06 <jp.bois.06@outlook.fr>
2021-08-27 12:55:24 +0200

    Project setup

---
+++ src/bootstrap.ts
@@ -0,0 +1,32 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+import { AppModule } from './modules/app.module';
+
+export const bootstrap = async (): Promise<INestApplication> => {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      forbidUnknownValues: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Backend sandbox')
+    .setDescription('The backend sandbox description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('', app, document);
+
+  const port = process.env.PORT || 3001;
+  await app.listen(port);
+
+  return app;
+};
